bc697c6d3d7eab8d160808ee8ef07e14d5745828
deanbgarlick <deanbgarlick@gmail.com>
2020-12-28 19:24:54 -0800

    Remove unnecessary run command

--- run_commands.sh
+++
@@ -1,8 +0,0 @@
-python deep_sort/tools/generate_detections.py --model=mars-small128.pb --mot_dir=./data --output_dir=./detections
-
-python deep_sort/deep_sort_app.py \
-    --sequence_dir=./data/sequence-1 \
-    --detection_file=./detections/sequence-1.npy \
-    --min_confidence=0.3 \
-    --nn_budget=100 \
-    --display=True
